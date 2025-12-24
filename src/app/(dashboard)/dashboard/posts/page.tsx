beeed4aff0d527dcef837a7b4f5556531b362551
Hemant Yadav <yad.heman@gmail.com>
2025-12-25 03:25:43 +0530

    feat(myposts): enhance empty state with creation prompt and description

--- src/app/(dashboard)/dashboard/posts/page.tsx
+++ src/app/(dashboard)/dashboard/posts/page.tsx
@@ -1,5 +1,16 @@
 import PostCard from '@/components/PostCard';
+import { Button } from '@/components/ui/button';
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from '@/components/ui/empty';
 import { getMyPosts } from '@/lib/blog';
+import { SquarePenIcon } from 'lucide-react';
+import Link from 'next/link';
 import Pagination from './components/Pagination';
 import SortingSelect from './components/SortingSelect';
 
@@ -35,23 +46,45 @@ async function MyPosts({
       <h1 className="flex flex-wrap items-center justify-between gap-4 text-2xl font-semibold md:text-3xl">
         Posts <SortingSelect />
       </h1>
-      <section className="divide-border bg-card divide-y rounded-xl border shadow-md">
-        {posts.map((post) => (
-          <div key={post._id.toString()}>
-            <PostCard post={post} user={user} />
-          </div>
-        ))}
-      </section>
-      <Pagination
-        totalPosts={totalPosts}
-        totalPages={totalPages}
-        page={page}
-        limit={limit}
-        hasNextPage={hasNextPage}
-        hasPrevPage={hasPrevPage}
-        nextPage={nextPage ?? page}
-        prevPage={prevPage ?? page}
-      />
+      {totalPosts === 0 ? (
+        <Empty className="from-muted/50 to-background h-full bg-gradient-to-b from-30%">
+          <EmptyHeader>
+            <EmptyMedia variant="icon">
+              <SquarePenIcon />
+            </EmptyMedia>
+            <EmptyTitle>No posts yet</EmptyTitle>
+            <EmptyDescription>
+              You haven&apos;t created any posts yet. Start writing your first
+              post to share your thoughts with the world.
+            </EmptyDescription>
+          </EmptyHeader>
+          <EmptyContent>
+            <Button variant="outline" asChild>
+              <Link href={`/write`}>Create Post</Link>
+            </Button>
+          </EmptyContent>
+        </Empty>
+      ) : (
+        <>
+          <section className="divide-border bg-card divide-y rounded-xl border shadow-md">
+            {posts.map((post) => (
+              <div key={post._id.toString()}>
+                <PostCard post={post} user={user} />
+              </div>
+            ))}
+          </section>
+          <Pagination
+            totalPosts={totalPosts}
+            totalPages={totalPages}
+            page={page}
+            limit={limit}
+            hasNextPage={hasNextPage}
+            hasPrevPage={hasPrevPage}
+            nextPage={nextPage ?? page}
+            prevPage={prevPage ?? page}
+          />
+        </>
+      )}
     </div>
   );
 }
